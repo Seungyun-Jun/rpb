11a5a5a550d27bf525bc26037304ea3c32344c84
Seungyun-Jun <lucastf1501@gmail.com>
2026-04-02 22:51:10 +0900

    Updated calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,23 @@
 def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    z = divide(x,y)
+    if z != None:
+        print("%d / %d = %0.3f" % (x, y, z))
+    
+    
+def add(a, b):
+    return a + b
+    
+def divide(a,b):
+    if b == 0:
+        print('Error: cannot divide by zero.')
+    else :
+        return a/b
+        
 
-def add():
-
-def divide():
+if __name__ == "__main__":
+    main()
